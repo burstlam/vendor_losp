8a231b86f4c8ececdc55beb032e560fdb845d17b
PeterCxy <xqsx43cxy@126.com>
2013-08-15 09:55:49 +0800

    losp: fix crash

--- config/common.mk
+++ config/common.mk
@@ -11,7 +11,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=false
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.selinux=1
+    ro.build.selinux=0
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
